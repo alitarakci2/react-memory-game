921412fdd829e10ffa5466ee2654b3b030d1490b
Ali <101870797+alitarakci2@users.noreply.github.com>
2023-02-26 13:41:25 +0300

    Adding Grid system and css

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import { useState, useEffect } from "react";
 import cardImages from "./Components/Images";
 import './App.css'
 import Header from "../src/Components/Header";
+import Grid from "../src/Components/Grid";
+
 
 
 const App = (props) => {
     const [cards, setCards] = useState([]);
     const [turns, setTurns] = useState(0);
+    const [choiceOne, setChoiceOne] = useState(null);
+    const [choiceTwo, setChoiceTwo] = useState(null);
+    const [disabled, setDisabled] = useState(false);
+
 
     const shuffleCards = () => {
         const shuffledCards = [...cardImages, ...cardImages]
@@ -20,9 +26,16 @@ const App = (props) => {
         shuffleCards();
     }, []);
 
+    const handleChoice = (card) => {
+        choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+
+    };
+
     return (
         <div>
            <Header turns={turns} onShuffle={shuffleCards}/>
+           <Grid cards={cards} choiceOne={choiceOne} choiceTwo={choiceTwo} disabled={disabled} handleChoice={handleChoice}/>
+           
         </div>
     )
 }
